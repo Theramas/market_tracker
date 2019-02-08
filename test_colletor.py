3ad81ba7a187d70ff840c07d8ed44a3c4caee2d6
Theramas <n.kalmikov@scalr.com>
2019-02-08 16:18:10 +0200

    Add xml writer test

--- test_colletor.py
+++ test_colletor.py
@@ -1,7 +1,8 @@
 import logging
 import unittest
 import sqlite3
-from unittest.mock import MagicMock
+import csv
+import os
 
 import collector
 
@@ -20,6 +21,7 @@ class TestCollector(unittest.TestCase):
         except:
             pass
         cls.connection.close()
+        os.remove('test.csv')
 
     def test_web_scrapping_success(self):
         url = "https://ca.finance.yahoo.com/screener/predefined/day_gainers?guccounter=1"
@@ -38,6 +40,19 @@ class TestCollector(unittest.TestCase):
         result = self.cursor.fetchall()
         self.assertEqual(
             result, [('test', 'test', 'test', 'test', 'test', 'test', 'test', 'test')])
+
+    def test_get_top(self):
+        data_from_function = collector.get_top_choices_from_database('gainers', 'change', 1)[0]
+        self.cursor.execute("SELECT name, MAX(change) from gainers")
+        data_from_database = self.cursor.fetchone()
+        self.assertEqual(data_from_database, data_from_function)
+
+    def tests_write_to_xml(self):
+        collector.write_to_xml([[1, 2, 3, 4]], 'test')
+        with open('test.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            third_row_text = ' '.join(list(reader)[2])
+        self.assertEqual(third_row_text, '1,2,3,4')
 
 
 if __name__ == '__main__':
